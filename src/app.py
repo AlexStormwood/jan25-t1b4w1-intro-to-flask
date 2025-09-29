6e26eeeef0fbc66faf1dab56e8be2fb9199dc056
Alex <alex.holder@bigfootds.com>
2025-09-29 20:02:34 +1000

    feat: json responses and post route and bruno implemented

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -8,3 +8,30 @@ print(__name__)
 def hello_world():
     return "<p>Hello, pizza!</p>"
 
+
+# http://localhost:5000/articles/
+@app.route("/articles")
+def article_demo_function():
+    # 1. Query the database for articles
+    # 2. Process the result to make sure it's a list of data 
+    # 3. Return the list 
+
+    return jsonify([
+        {
+            "title":"Example Article 1",
+            "content":"Blah blah blah",
+            "author":"130489u23topi2"
+        },
+        {
+            "title":"Example Article 2",
+            "content":"Blah 2 blah blah 2",
+            "author":"3405398urfgop"
+        }
+    ])
+
+
+@app.route("/bananas", methods=["POST"])
+def post_bananas():
+    return jsonify({
+        "message":"Post bananas!"
+    })
